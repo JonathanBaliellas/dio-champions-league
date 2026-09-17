7935b986cc90ce67ea5f6cae59876e090f711fd3
Jonathan Baliellas <101955148+JonathanBaliellas@users.noreply.github.com>
2026-09-16 21:56:43 -0300

    refactor(players): move business logic from controller to service

--- src/controllers/player.controller.ts
+++ src/controllers/player.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
+import { getPlayersService } from "../services/player.service";
 
-export const getPlayers = (req: Request, res: Response) => {
-    res.status(200).json({ player: "Jontz" });
+export const getPlayers = async (req: Request, res: Response) => {
+    const data = await getPlayersService();
+    res.status(200).json(data);
 };
---
+++ src/services/player.service.ts
@@ -0,0 +1,3 @@
+export const getPlayersService = async () => {
+    return { player: 'Jontz' };
+};
