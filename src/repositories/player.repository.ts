4d4484094447b86e6cf88a630e22b261df1f4878
Jonathan Baliellas <101955148+JonathanBaliellas@users.noreply.github.com>
2026-09-19 00:23:49 -0300

    feat(player): expand PlayerModel properties and update mock players list
    - Add club, nationality, position, and nested statistics object to PlayerModel interface
    - Populate PLAYERS constant with 5 players matching the updated data structure"

--- src/repositories/player.repository.ts
+++ src/repositories/player.repository.ts
@@ -1,11 +1,86 @@
 import { PlayerModel } from "../models/PlayerModel";
 
 const database: PlayerModel[] = [
-    { id: 1, name: "Erling Haaland" },
-    { id: 2, name: "Kylian Mbappé" },
-    { id: 3, name: "Vinícius Júnior" },
-    { id: 4, name: "J Jude Bellingham" },
-    { id: 5, name: "Harry Kane" }
+    {
+        id: 1,
+        name: 'Lionel Messi',
+        club: 'Inter Miami',
+        nationality: 'Argentina',
+        position: 'RW',
+        statistics: {
+        overall: 90,
+        pace: 80,
+        shooting: 87,
+        passing: 90,
+        dribbling: 94,
+        defending: 33,
+        physical: 64
+        }
+    },
+    {
+        id: 2,
+        name: 'Cristiano Ronaldo',
+        club: 'Al-Nassr',
+        nationality: 'Portugal',
+        position: 'ST',
+        statistics: {
+        overall: 86,
+        pace: 77,
+        shooting: 88,
+        passing: 75,
+        dribbling: 80,
+        defending: 34,
+        physical: 75
+        }
+    },
+    {
+        id: 3,
+        name: 'Kylian Mbappé',
+        club: 'Real Madrid',
+        nationality: 'França',
+        position: 'ST',
+        statistics: {
+        overall: 91,
+        pace: 97,
+        shooting: 90,
+        passing: 80,
+        dribbling: 92,
+        defending: 36,
+        physical: 78
+        }
+    },
+    {
+        id: 4,
+        name: 'Kevin De Bruyne',
+        club: 'Manchester City',
+        nationality: 'Bélgica',
+        position: 'CM',
+        statistics: {
+        overall: 91,
+        pace: 72,
+        shooting: 88,
+        passing: 94,
+        dribbling: 87,
+        defending: 65,
+        physical: 78
+        }
+    },
+    {
+        id: 5,
+        name: 'Virgil van Dijk',
+        club: 'Liverpool',
+        nationality: 'Holanda',
+        position: 'CB',
+        statistics: {
+        overall: 89,
+        pace: 78,
+        shooting: 60,
+        passing: 71,
+        dribbling: 72,
+        defending: 89,
+        physical: 86
+        }
+    }
 ];
 
 export const findAllPlayers = async (): Promise<PlayerModel[]> => {
