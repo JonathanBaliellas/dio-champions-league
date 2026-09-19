b3237e73d04e627dfb9fc02bfe726258a8f88c4b
Jonathan Baliellas <101955148+JonathanBaliellas@users.noreply.github.com>
2026-09-19 00:59:57 -0300

    feat(player): feat(player): implement get player by ID endpoint

--- src/controllers/player.controller.ts
+++ src/controllers/player.controller.ts
@@ -1,7 +1,13 @@
 import { Request, Response } from "express";
-import { getPlayersService } from "../services/player.service";
+import { getPlayerByIdService, getPlayersService } from "../services/player.service";
 
 export const getPlayers = async (req: Request, res: Response) => {
     const httpResponse = await getPlayersService();
+    res.status(httpResponse.statusCode).json(httpResponse.body);
+};
+
+export const getPlayerById = async (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    const httpResponse = await getPlayerByIdService(id);
     res.status(httpResponse.statusCode).json(httpResponse.body);
 };
--- src/services/player.service.ts
+++ src/services/player.service.ts
@@ -1,12 +1,22 @@
-import { findAllPlayers } from "../repositories/player.repository";
-import { noContent, ok } from "../utils/http-helper";
+import * as PlayerRepository from "../repositories/player.repository";
+import * as HttpResponse from "../utils/http-helper";
 
 export const getPlayersService = async () => {
-    const data = await findAllPlayers();
+    const data = await PlayerRepository.findAllPlayers();
     let response = null;
 
-    if (data) response = await ok(data);
-    else response = await noContent();
+    if (data) response = await HttpResponse.ok(data);
+    else response = await HttpResponse.noContent();
     
+    return response;
+};
+
+export const getPlayerByIdService = async (id: number) => {
+    const data = await PlayerRepository.getPlayerById(id);
+    let response = null;
+
+    if (data) response = HttpResponse.ok(data);
+    else response = HttpResponse.noContent();
+
     return response;
 };
